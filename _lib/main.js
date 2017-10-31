2981d5b8bb675e91e34f9b9d22abab1797280f00
Li Xueli <mixj93@163.com>
2017-10-31 13:11:42 +0800

    hide content in index page

--- _lib/main.js
+++ _lib/main.js
@@ -2,10 +2,9 @@ window.onload = changeBodyHeight
 window.onhashchange = changeBodyHeight
 
 function changeBodyHeight() {
-  var path = window.location.pathname
   var hash = window.location.hash
   var bodyEle = document.getElementsByTagName('body')[0]
-  if (path === '/' && hash === '#/') {
+  if (hash === '#/') {
     bodyEle.style.overflowY = 'hidden'
   } else {
     bodyEle.style.overflowY = 'auto'
